e1786f87b518dc4b2edc19521e5cfe7489b87ffb
Kingsley Eze <firebox@ikingsley.com>
2026-02-09 23:35:46 +0100

    chore: bump version to 1.0.4

--- lib/src/flagkit_client.dart
+++ lib/src/flagkit_client.dart
@@ -866,7 +866,7 @@ class FlagKitClient {
         httpClient: _httpClient,
         sessionId: sessionId,
         environmentId: _environmentId ?? '',
-        sdkVersion: '1.0.3',
+        sdkVersion: '1.0.4',
         config: EventQueueConfig(
           batchSize: options.eventBatchSize,
           flushIntervalMs: options.eventFlushInterval.inMilliseconds,
